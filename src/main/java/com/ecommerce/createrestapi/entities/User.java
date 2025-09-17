9d40a4e5b4fbc376e5b7fe82c688b6d5562a2ad7
Pavan Vineethreddy Thaviti <vineeth.thaviti@gmail.com>
2025-09-17 16:24:49 -0500

    Removed unused code and changed Object to string for type ID and mongodb class

--- src/main/java/com/ecommerce/createrestapi/entities/User.java
+++ src/main/java/com/ecommerce/createrestapi/entities/User.java
@@ -2,19 +2,20 @@ package com.ecommerce.createrestapi.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.stereotype.Service;
+
 
 @Document(collection = "users")
-@AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
-    private ObjectId id;
+    private String id;
     private String username;
     private String firstname;
     private String lastname;
--- src/main/java/com/ecommerce/createrestapi/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/createrestapi/repositories/UserRepository.java
@@ -2,5 +2,5 @@ package com.ecommerce.createrestapi.repositories;
 import com.ecommerce.createrestapi.entities.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-public interface UserRepository extends MongoRepository<User, ObjectId> {
+public interface UserRepository extends MongoRepository<User, String> {
 }
